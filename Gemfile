6970bba73494857788e9aa96cf7e7077a16a5fb4
Jesse Goodnoe <jesse@pike13.com>
2019-11-05 13:39:33 -0800

    Rubocop cleanup

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 source 'http://rubygems.org'
 
-gem 'sinatra'
 gem 'oauth2'
+gem 'sinatra'
--- lib/server.rb
+++ lib/server.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'oauth2'
 require 'json'
@@ -7,36 +9,36 @@ $consumer_key = ENV['P13_OAUTH_KEY']
 $consumer_secret = ENV['P13_OAUTH_SECRET']
 $pike13_host = ENV['P13_HOST']
 
-raise "Set env var P13_HOST to something like http://my-biz.pike13.com" unless $pike13_host
-raise "Must set env var P13_OAUTH_KEY" unless $consumer_key
-raise "Must set env var P13_OAUTH_SECRET" unless $consumer_secret
+raise 'Set env var P13_HOST to something like http://my-biz.pike13.com' unless $pike13_host
+raise 'Must set env var P13_OAUTH_KEY' unless $consumer_key
+raise 'Must set env var P13_OAUTH_SECRET' unless $consumer_secret
 
 # Tokens should also work without subdomain
 def client
-  OAuth2::Client.new($consumer_key, $consumer_secret, :site => $pike13_host)
+  OAuth2::Client.new($consumer_key, $consumer_secret, site: $pike13_host)
 end
 
-get "/" do
+get '/' do
   erb :root
 end
 
-get "/auth/test" do
-  auth_url = client.auth_code.authorize_url(:redirect_uri => redirect_uri)
+get '/auth/test' do
+  auth_url = client.auth_code.authorize_url(redirect_uri: redirect_uri)
   puts "Redirecting the browser to: #{auth_url}"
   redirect auth_url
 end
 
 get '/auth/test/callback' do
-  auth_code = params["code"]
+  auth_code = params['code']
   puts "Your auth_code is: #{auth_code}"
 
   if auth_code.nil?
-    @message = "Authorization request was denied."
+    @message = 'Authorization request was denied.'
     puts @message
     erb :fail
   else
-    puts "Will now use the auth_code to retrieve the access_token."
-    access_token = client.auth_code.get_token(auth_code, :redirect_uri => redirect_uri)
+    puts 'Will now use the auth_code to retrieve the access_token.'
+    access_token = client.auth_code.get_token(auth_code, redirect_uri: redirect_uri)
     session[:access_token] = access_token.token
     puts "Access token: #{session[:access_token]}"
     @message = session[:access_token]
@@ -45,7 +47,7 @@ get '/auth/test/callback' do
 end
 
 get '/account' do
-  @message = get_response("/api/v2/account.json")
+  @message = get_response('/api/v2/account.json')
   erb :account
 end
 
